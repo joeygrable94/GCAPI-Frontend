853d300f7f954c7f6081874e709244828d58a5f0
Joey Grable <hello@joeygrable.com>
2023-01-29 19:10:13 -0800

    updated backend api url setting in frontend utils

--- src/lib/core/utils.ts
+++ src/lib/core/utils.ts
@@ -76,8 +76,8 @@ export const encode = encodeURIComponent;
  */
 export const API_PROTOCOL: string =
   import.meta.env.VITE_TLS_ACTIVE === 'true' ? 'https' : 'http';
-export const API_HOST: string = import.meta.env.DEV
-  ? import.meta.env.VITE_API_BASE_URL_DEV
-  : import.meta.env.VITE_API_BASE_URL_LIVE;
+export const API_HOST: string = import.meta.env.VITE_API_BASE_URL
+  ? import.meta.env.VITE_API_BASE_URL
+  : 'localhost:8888';
 export const API_URL_BASE = `${API_PROTOCOL}://${API_HOST}`;
 OpenAPI.BASE = API_URL_BASE;
