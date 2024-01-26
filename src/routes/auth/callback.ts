cb3274d002abb7730d56216f102002ce3ec16e92
Joey Grable <hello@joeygrable.com>
2024-01-26 15:36:34 -0800

    changed routes'

--- src/routes/auth/callback.ts
+++ src/routes/auth/callback.ts
@@ -1,23 +1,13 @@
 'use server';
-import { APIEvent, parseCookies } from '@solidjs/start/server';
-// import { UserSessionData, getSession } from '~/api/server';
+import { APIEvent, parseCookies, sendRedirect, setCookie } from '@solidjs/start/server';
+import { AES } from 'crypto-js';
 import { auth0FetchOAuthToken, auth0UserInfo } from '~/components';
-import { UserInfo, getSession } from '~/server/session';
+import { UserInfo, UserSessionData, getSession } from '~/server/session';
 
 export async function GET(event: APIEvent) {
-  console.log('Auth0 callback');
-  console.log('Event', typeof event);
-  console.log('Event', event.path);
-  console.log('event.path', event.request.url);
-
   let baseUrl = import.meta.env.VITE_APP_BASE_URL;
-  if (import.meta.env.VITE_DEBUG) {
-    console.log('baseUrl', baseUrl);
-  }
-
+  const session = await getSession(event);
   const url = new URL(event.request.url);
-  // const session = await getSession(event);
-  const headers = new Headers();
   const code: string | null = url.searchParams.get('code');
   const state: string | null = url.searchParams.get('state');
   const cookies = parseCookies(event);
@@ -25,12 +15,14 @@ export async function GET(event: APIEvent) {
 
   if (!verification) {
     console.error('No verification cookie found');
-    return new Response(JSON.stringify({ error: 'verification failed' }), {
-      status: 500
-    });
+    await session.clear();
+    return sendRedirect(event, '/login', 500);
   }
 
   if (import.meta.env.VITE_DEBUG) {
+    console.log('Auth0 callback');
+    console.log('event.path', event.request.url);
+    console.log('baseUrl', baseUrl);
     console.log('state verification');
     console.log(state);
     console.log(verification);
@@ -38,26 +30,20 @@ export async function GET(event: APIEvent) {
 
   if (code === undefined || code === null) {
     console.error('No code found');
-    return new Response(JSON.stringify({ error: 'missing code' }), {
-      status: 500
-    });
+    await session.clear();
+    return sendRedirect(event, '/login', 500);
   }
 
   if (state === undefined || state === null) {
     console.error('No auth state found');
-    return new Response(JSON.stringify({ error: 'missing state' }), {
-      status: 500
-    });
+    await session.clear();
+    return sendRedirect(event, '/login', 500);
   }
 
   if (state !== verification.state) {
     console.error('Code and state do not match verification');
-    return new Response(
-      JSON.stringify({ error: 'code and state verification failed' }),
-      {
-        status: 403
-      }
-    );
+    await session.clear();
+    return sendRedirect(event, '/login', 403);
   }
 
   let redirectUrl = import.meta.env.VITE_AUTH0_REDIRECT_URI;
@@ -88,39 +74,38 @@ export async function GET(event: APIEvent) {
     console.log(userInfo);
   }
   if (userInfo === undefined) {
-    return new Response(JSON.stringify({ error: 'unauthorized' }), {
-      status: 401
-    });
+    await session.clear();
+    return sendRedirect(event, '/login', 401);
   }
 
-  const session = await getSession(event);
-  const sessionUser = {
-    sub: userInfo.sub,
-    nickname: userInfo.nickname,
-    name: userInfo.name,
-    picture: userInfo.picture,
-    updated_at: userInfo.updated_at,
-    email: userInfo.email,
-    email_verified: userInfo.email_verified
-  } as UserInfo;
   const newSession = {
     accessToken: jsonAuthToken.access_token,
     refreshToken: jsonAuthToken.refresh_token,
-    idToken: jsonAuthToken.id_token,
-    scope: jsonAuthToken.scope,
     tokenType: jsonAuthToken.token_type,
-    userInfo: sessionUser,
-    userId: userInfo.sub,
-    permissions: userInfo['https://github.com/dorinclisu/fastapi-auth0/roles'] ?? []
-  };
-
-  await session.clear();
+    idToken: jsonAuthToken.id_token,
+    user: {
+      sub: userInfo.sub,
+      nickname: userInfo.nickname,
+      name: userInfo.name,
+      picture: userInfo.picture,
+      updated_at: userInfo.updated_at,
+      email: userInfo.email,
+      email_verified: userInfo.email_verified
+    } as UserInfo
+  } as UserSessionData;
   await session.update((d) => {
     d = newSession;
     return d;
   });
-  console.log('Session: ', session.id, session.data);
 
-  // return sendRedirect(event, '/', 302);
-  return event.respondWith(new Response(JSON.stringify({ success: true })));
+  let newState = {
+    accessToken: jsonAuthToken.access_token,
+    refreshToken: jsonAuthToken.refresh_token,
+    tokenType: jsonAuthToken.token_type,
+    idToken: jsonAuthToken.id_token
+  };
+  setCookie(event, 'gcapi_auth', JSON.stringify(newState), {
+    encode: (v) => AES.encrypt(v, import.meta.env.VITE_SESSION_SECRET).toString()
+  });
+  return sendRedirect(event, '/', 302);
 }
---
+++ src/routes/login.tsx
@@ -0,0 +1,16 @@
+import { Button } from 'solid-bootstrap';
+import { Component } from 'solid-js';
+import { useAuth0 } from '~/components';
+
+const Login: Component = () => {
+  const [authState, authAct] = useAuth0();
+
+  return (
+    <main>
+      <h1>Login</h1>
+      <Button onClick={async () => await authAct.authorize()}>Login</Button>
+    </main>
+  );
+};
+
+export default Login;
