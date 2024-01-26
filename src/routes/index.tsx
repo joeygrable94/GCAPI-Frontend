f302dd551ee4f6d51f27e979bc3cdeb508434e7d
Joey Grable <hello@joeygrable.com>
2024-01-26 09:42:59 -0800

    added base routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'solid-js';
+
+const Home: Component = () => {
+  return (
+    <main>
+      <h1>GCAPI Auth0 Secured Backend</h1>
+      <p>Welcome to your dashboard.</p>
+    </main>
+  );
+};
+
+export default Home;
