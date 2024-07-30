5ab55ec9fdf572b139b755816a9242793635feb7
Joey Grable <hello@joeygrable.com>
2024-07-30 14:05:03 -0700

    minor edit

--- src/routes/authorized.tsx
+++ src/routes/authorized.tsx
@@ -2,12 +2,10 @@ import { protected$ } from '@solid-mediakit/auth';
 
 export default protected$(
   (session$) => {
-    // const res = protectedQuery(() => ({ hello: 'world' }))
     return (
       <div>
         <h1>Protected Page</h1>
         <p>Session: {JSON.stringify(session$)}</p>
-        {/* <p>Response: {JSON.stringify(res.data)}</p> */}
       </div>
     );
   },
